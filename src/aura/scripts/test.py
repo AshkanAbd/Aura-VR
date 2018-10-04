7f7c540dfd0598532845263517440e72ef6c2622
AshkanAbd <kotksthegame@gmail.com>
2018-10-04 15:27:17 +0330

    ashkan

--- src/aura/scripts/test.py
+++ src/aura/scripts/test.py
@@ -1,46 +1,10 @@
 #!/usr/bin/env python
 
-import math
-<<<<<<< HEAD
-import rospy
-import numpy as np
-import nav_msgs.msg
-import tf.transformations
-
-
-def get_map(map):
-    global main_map
-    h = map.info.height
-    w = map.info.width
-    new_map = np.asarray(map.data)
-    reshape = new_map.reshape(h, w)
-    main_map = map
-    print(convert_from_robot_to_map(0,-2))
-
-
-def get_odom(odom):
-    q = (
-        odom.pose.pose.orientation.x,
-        odom.pose.pose.orientation.y,
-        odom.pose.pose.orientation.z,
-        odom.pose.pose.orientation.w
-    )
-    print((tf.transformations.euler_from_quaternion(q)[2]) * 180 / math.pi)
-
-
-def convert_from_robot_to_map(robot_y, robot_x):
-    global main_map
-    map_x = (robot_x - main_map.info.origin.position.x) // main_map.info.resolution
-    map_y = (robot_y - main_map.info.origin.position.y) // main_map.info.resolution
-    return map_y, map_x
-
-=======
 import cv2 as cv
 import numpy as np
 import rospy
 import time
 import geometry_msgs.msg
->>>>>>> d8e967595b87e6c2f8621474fec2393c79a9bfb0
 
 
 def publishing(msg):
