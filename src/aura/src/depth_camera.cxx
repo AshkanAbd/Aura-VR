e315612c56fb8838dc852cac9575a614997161e8
AshkanAbd <kotksthegame@gmail.com>
2018-10-07 02:17:45 +0330

    depth

--- src/aura/src/depth_camera.cxx
+++ src/aura/src/depth_camera.cxx
@@ -11,8 +11,21 @@
 #include <map>
 #include <algorithm>
 
-static
+const static std::string name_space = "robot0";
+
+
+void get_depth_image(const sensor_msgs::Image &img) {
+    cv::Mat frame;
+    cv_bridge::CvImagePtr img_ptr = cv_bridge::toCvCopy(img, sensor_msgs::image_encodings::TYPE_32FC1);
+    frame = img_ptr->image;
+    cv::imshow("f", frame);
+    cv::waitKey(1);
+}
 
 int main(int argc, char **argv) {
-
+    ros::init(argc, argv, "depth_camera");
+    ros::NodeHandle node_handle;
+    ros::Subscriber depth_subscriber = node_handle.subscribe("/" + name_space + "/camera_depth/depth/image_raw", 10,
+                                                             get_depth_image);
+    ros::spin();
 }
