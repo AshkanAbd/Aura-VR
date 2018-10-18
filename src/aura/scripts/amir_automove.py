01ae0f582e935ed4a5ce3d5f067de5bd3794a46e
Amir <amir.hj1998620@gmail.com>
2018-10-18 21:02:49 +0330

    amir

--- src/aura/scripts/amir_automove.py
+++ src/aura/scripts/amir_automove.py
@@ -42,14 +42,14 @@ def convert_from_map_to_robot(map_y, map_x):
 
 
 def cluster(group: aura.msg.group):
-    global robot_pose_x, robot_pose_y, map_height, map_width, blocks
+    global robot_pose_x, robot_pose_y, map_height, map_width, blocks, blocks_list
     blocks = group
     robot_block_index = find_robot_cluster()
     upper_block_index = get_up_cluster()
     lower_block_index = get_down_cluster()
     left_block_index = get_left_cluster()
     right_block_index = get_right_cluster()
-    # print(robot_block_index,upper_block_index,lower_block_index,left_block_index,right_block_index)
+    blocks_list = [upper_block_index, lower_block_index, right_block_index, left_block_index]
 
 
 def find_robot_cluster():
@@ -64,25 +64,25 @@ def find_robot_cluster():
 
 def get_up_cluster():
     global robot_block_index, y, x, upper_cluster
-    upper_cluster = (y - 1) * 16 + x
+    upper_cluster = int((y - 1) * 16 + x)
     return upper_cluster
 
 
 def get_down_cluster():
     global robot_block_index, y, x, lower_cluster
-    lower_cluster = (y + 1) * 16 + x
+    lower_cluster = int((y + 1) * 16 + x)
     return lower_cluster
 
 
 def get_right_cluster():
     global robot_block_index, y, x, right_cluster
-    right_cluster = ((y) * 16 + x) + 1
+    right_cluster = int(((y) * 16 + x) + 1)
     return right_cluster
 
 
 def get_left_cluster():
     global robot_block_index, y, x, left_cluster
-    left_cluster = ((y) * 16 + x) - 1
+    left_cluster = int(((y) * 16 + x) - 1)
     return left_cluster
 
 
@@ -93,32 +93,59 @@ def setup_move_base():
     move_base_goal = move_base_msgs.msg.MoveBaseGoal()
 
 
-def generate_goal():
+def generating_goal():
     global robot_block, map_height, map_width, robot_clu, robot_block_ind, robot_clu, is_published, robot_pose_x, robot_pose_y
-    while False in is_published: pass
-    robot_block_ind = find_robot_cluster()
-    print(robot_block_ind)
     robot_pose = convert_from_robot_to_map(robot_pose_y, robot_pose_x)
     y = robot_pose[0] // (map_height // 16)
     x = robot_pose[1] // (map_width // 16)
     y1 = (map_width // 16) * (x)
     x1 = (map_height // 16) * (y)
-    reshaped_robot_cluster = np.asarray(blocks.array[robot_block_ind].data).reshape(map_width // 16, map_width // 16)
-    roboty, robotx = convert_from_robot_to_map(robot_odometry.pose.pose.position.y, robot_odometry.pose.pose.position.x)
+    # roboty, robotx = convert_from_robot_to_map(robot_odometry.pose.pose.position.y, robot_odometry.pose.pose.position.x)
     n_shown = np.where(reshaped_robot_cluster == -1)
     rand_celly = random.randint(0, len(n_shown[0]))
     rand_cellx = random.randint(0, len(n_shown[1]))
-    # goal_x , goal_y = convert_from_map_to_robot(y1 + n_shown[0][rand_cell] , x1 +n_shown[1][rand_cell])
-    # move_base_clinet(goal_x , goal_y)
-    print(len(n_shown[0]))
-    if(len(n_shown[0]!=0)):
+    if (len(n_shown[0] != 0)):
         goal_x, goal_y = convert_from_map_to_robot(y1 + n_shown[0][rand_celly], x1 + n_shown[1][rand_cellx])
-    # print(y1 + n_shown[0][rand_celly], x1 + n_shown[1][rand_cellx])
-    # print(goal_y, goal_x)
-    # print(roboty, robotx)
         move_base_clinet(goal_y, goal_x)
+
+
+def sending_goal():
+    global robot_block_ind, is_published, map_height, map_width, reshaped_robot_cluster, blo
+    while False in is_published: pass
+    robot_block_ind = find_robot_cluster()
+    print(robot_block_ind)
+    reshaped_robot_cluster = np.asarray(blocks.array[robot_block_ind].data).reshape(map_width // 16, map_width // 16)
+    if len(reshaped_robot_cluster[reshaped_robot_cluster == -1]) > 0:
+        # print(robot_block_ind)
+        generating_goal()
     else:
-        exit()
+        random.shuffle(blocks_list)
+        robot_block_ind = blocks_list[0]
+        print(blocks_list)
+        print(robot_block_ind)
+        reshaped_robot_cluster = np.asarray(blocks.array[robot_block_ind].data).reshape(map_width // 16,
+                                                                                       map_width // 16)
+        if len(reshaped_robot_cluster[reshaped_robot_cluster == -1]) > 0:
+            generating_goal()
+
+    # robot_pose = convert_from_robot_to_map(robot_pose_y, robot_pose_x)
+    # y = robot_pose[0] // (map_height // 16)
+    # x1 = (map_height // 16) * (y)
+    # reshaped_robot_cluster = np.asarray(blocks.array[robot_block_ind].data).reshape(map_width // 16, map_width // 16)
+    # roboty, robotx = convert_from_robot_to_map(robot_odometry.pose.pose.position.y, robot_odometry.pose.pose.position.x)
+
+    # x = robot_pose[1] // (map_width // 16)
+    # y1 = (map_width // 16) * (x)
+    # n_shown = np.where(reshaped_robot_cluster == -1)
+    # rand_celly = random.randint(0, len(n_shown[0]))
+    # rand_cellx = random.randint(0, len(n_shown[1]))
+    # if(len(n_shown[0]!=0)):
+    #     goal_x, goal_y = convert_from_map_to_robot(y1 + n_shown[0][rand_celly], x1 + n_shown[1][rand_cellx])
+    #     move_base_clinet(goal_y, goal_x)
+    # else:
+    #     print(blocks_list)
+    #     robot_block_ind = random.choice(blocks_list)
+
 
 # goal status--- PENDING=0--- ACTIVE=1---PREEMPTED=2--SUCCEEDED=3--ABORTED=4---REJECTED=5--PREEMPTING=6---RECALLING=7---RECALLED=8---LOST=9
 def goal_status(data1, data2):
@@ -126,7 +153,7 @@ def goal_status(data1, data2):
     if (data1 == 1):
         pass
     elif (data1 == 2 or data1 == 3 or data1 == 4 or data1 == 5):
-        generate_goal()
+        sending_goal()
     else:
         pass
 
@@ -157,7 +184,6 @@ robot_clu = None
 map_width = None
 map_height = None
 robot_block_ind = None
-# xx = None
 blocks = aura.msg.group
 
 if __name__ == '__main__':
@@ -167,5 +193,5 @@ if __name__ == '__main__':
     rospy.Subscriber('/core/map', nav_msgs.msg.OccupancyGrid, get_map)
     rospy.Subscriber('/core/blocks', aura.msg.group, cluster)
     rospy.Subscriber('/' + namespace + '/odom', nav_msgs.msg.Odometry, get_robot_odom)
-    generate_goal()
+    sending_goal()
     rospy.spin()
