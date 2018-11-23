dbc476c7be464ea4cb2ba5b2ab18564556fc62f9
AshkanAbd <kotksthegame@gmail.com>
2018-11-24 01:34:35 +0330

    merge map

--- src/aura/scripts/core.py
+++ src/aura/scripts/core.py
@@ -13,20 +13,23 @@ class CoreMapBuilder:
     robot2 = 'robot2'
     robot3 = 'robot3'
     core_map = np.array([])
+    tolerance_zero = {}
+    tolerance_one = {}
+    robot_evolution = {}
     main_map = nav_msgs.msg.OccupancyGrid()
     core_publisher = None
     rate = None
 
     def __init__(self, robot0='robot0', robot1='robot1', robot2='robot2', robot3='robot3'):
-        self.robot1 = robot0
-        self.robot2 = robot1
-        self.robot3 = robot2
-        self.robot0 = robot3
+        self.robot0 = robot0
+        self.robot1 = robot1
+        self.robot2 = robot2
+        self.robot3 = robot3
         rospy.init_node('core_builder')
-        rospy.Subscriber('/' + self.robot1 + '/map', nav_msgs.msg.OccupancyGrid, self.get_robots_map)
-        rospy.Subscriber('/' + self.robot2 + '/map', nav_msgs.msg.OccupancyGrid, self.get_robots_map)
-        rospy.Subscriber('/' + self.robot3 + '/map', nav_msgs.msg.OccupancyGrid, self.get_robots_map)
-        rospy.Subscriber('/' + self.robot0 + '/map', nav_msgs.msg.OccupancyGrid, self.get_robots_map)
+        rospy.Subscriber('/' + self.robot0 + '/map', nav_msgs.msg.OccupancyGrid, self.get_robots_map, '1')
+        rospy.Subscriber('/' + self.robot1 + '/map', nav_msgs.msg.OccupancyGrid, self.get_robots_map, '2')
+        rospy.Subscriber('/' + self.robot2 + '/map', nav_msgs.msg.OccupancyGrid, self.get_robots_map, '3')
+        rospy.Subscriber('/' + self.robot3 + '/map', nav_msgs.msg.OccupancyGrid, self.get_robots_map, '4')
         rospy.Subscriber('/core/out_map', aura.msg.group_int, self.get_out_map)
         self.core_publisher = rospy.Publisher('/core/map', nav_msgs.msg.OccupancyGrid, queue_size=100)
         self.rate = rospy.Rate(5)
@@ -37,11 +40,11 @@ class CoreMapBuilder:
             if self.core_map[out.data_int[0], out.data_int[1]] == -1:
                 self.core_map[out.data_int[0], out.data_int[1]] = 100
 
-    def get_robots_map(self, robot_map):
+    def get_robots_map(self, robot_map, robot_id):
         global map_info
         self.main_map = robot_map
         map_info = robot_map
-        self.build_core_map(robot_map)
+        self.build_core_map(robot_map, robot_id)
 
     def publish_to_core(self):
         while not rospy.is_shutdown():
@@ -54,14 +57,50 @@ class CoreMapBuilder:
             except Exception as e:
                 print(e)
 
-    def build_core_map(self, robot_map):
+    def build_core_map(self, robot_map, robot_id):
         global map_info
         reshaped_map = np.asarray(robot_map.data).reshape(robot_map.info.height, robot_map.info.width)
         if self.core_map.shape != reshaped_map.shape:
             self.core_map = reshaped_map.copy()
         map1 = reshaped_map.copy()
-        self.core_map[map1 == 0] = 0
-        self.core_map[map1 == 100] = 100
+        new_zero = np.where(map1 == 0)
+        new_zero_coo = []
+        for i in range(len(new_zero[0])):
+            new_zero_coo.append((new_zero[0][i], new_zero[1][i]))
+        new_one = np.where(map1 == 100)
+        new_one_coo = []
+        for i in range(len(new_one[0])):
+            new_one_coo.append((new_one[0][i], new_one[1][i]))
+        for coordinate in new_zero_coo:
+            if self.core_map[coordinate[0], coordinate[1]] == -1:
+                self.core_map[coordinate[0], coordinate[1]] = 0
+                self.robot_evolution[coordinate] = robot_id
+            elif self.core_map[coordinate[0], coordinate[1]] == 100:
+                if coordinate in self.robot_evolution and self.robot_evolution[coordinate] == robot_id:
+                    self.core_map[coordinate[0], coordinate[1]] = 0
+                else:
+                    if coordinate not in self.tolerance_zero:
+                        self.tolerance_zero[coordinate] = 1
+                    else:
+                        self.tolerance_zero[coordinate] += 1
+        for coordinate in new_one_coo:
+            if self.core_map[coordinate[0], coordinate[1]] == -1:
+                self.core_map[coordinate[0], coordinate[1]] = 100
+                self.robot_evolution[coordinate] = robot_id
+            elif self.core_map[coordinate[0], coordinate[1]] == 0:
+                if coordinate in self.robot_evolution and self.robot_evolution[coordinate] == robot_id:
+                    self.core_map[coordinate[0], coordinate[1]] = 100
+                else:
+                    if coordinate not in self.tolerance_one:
+                        self.tolerance_one[coordinate] = 1
+                    else:
+                        self.tolerance_one[coordinate] += 1
+        for pair in self.tolerance_one.keys():
+            if self.tolerance_one[pair] > 10 and self.core_map[pair[0], pair[1]] == 0:
+                self.core_map[pair[0], pair[1]] = 100
+        for pair in self.tolerance_zero.keys():
+            if self.tolerance_zero[pair] > 10 and self.core_map[pair[0], pair[1]] == 100:
+                self.core_map[pair[0], pair[1]] = 0
         map_info = self.core_map
 
 
